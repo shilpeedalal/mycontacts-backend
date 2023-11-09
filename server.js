06b9bc76022e15cdf498a6f17981cf1de35738c5
Shilpee <shilpeedalal937@gmail.com>
2023-11-09 19:04:36 +0530

    get endpoin added

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 
 //middleware
 app.use(express.json())
-// app.get('/', (req,res)=>{
-//     res.send("")
-// })
+app.get('/', (req,res)=>{
+    res.send("This is HomePage for the Contact Management System project. This project has only backend. Here is the github link and also postman collections link.")
+})
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"))
 app.use(errorHandler)
